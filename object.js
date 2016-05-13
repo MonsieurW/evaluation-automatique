7b4a25000521cdf346f3df73d77dc0863c3ff025
MonsieurW <adressedallan@gmail.com>
2016-05-13 12:50:03 +0200

    Systeme d'aide + revision footer+ correction bug système d'acquisition

--- object.js
+++ object.js
@@ -1,125 +1,179 @@
 
 
-latable='plantetest';latabletemp='plantetemptest';//plantetest,plantetemptest
+latable='plante2';latabletemp='plantetemp';//plantetest,plantetemptest
+//plante2/plantetemp
+
 
 //froid<span class="awsm">&#xf069;</span>
 
-titreschoisis=['noms','calendar','vies','enver','soleil','solriche','compagnons','autres']
 
 
-titres={//quelles sont les différentes cases et leurs contenus
-	noms:{desc:'Nom',chp:['nom','famille'],colorbox:'white'},
-	calendar:{desc:'Calendrier',chp:['semisext','semisabri','semisint','RECOLTE','repiq'],colorbox:'#3EF53E'},
-	vies:{
-		desc:'Durée de vie',chp:['vie','vivace','type'],colorbox:'#54CEAD'
-	},
-	compagnons:{
-		desc:'compagnonage',chp:['associe','antiassocie']
-	,colorbox:'#0FA500'},
-	solriche:{
-		desc:'Occupation du sol ',chp:['solriche','racine'],colorbox:'#FFA500'},
-	enver:{
-		desc:'Envergure',chp:['hauteur','largeur'],colorbox:'&#xf07d;'},
-	soleil:{
-		desc:'Ensoleillement ',chp:['soleil'],colorbox:'#54CEAD'
-	},
-	multip:{desc:'Multiplication',chp:['multi','Tmin'],colorbox:'#00C2FF'},
-	semis:{desc:'Semis',chp:['prof','drang','dligne'],colorbox:'#D8D5D5'},
-	sol:{desc:'Sol',chp:['pH','eau'],colorbox:'#E8ADAD'},
-	autres:{desc:'Autres',chp:['rendement','tpslevee','tpsconserv','utilisation','commentaire'],colorbox:'#E0ADAD'}
-		
-};
+if (typeof Object.create != 'function') {
+  // Production steps of ECMA-262, Edition 5, 15.2.3.5
+  // Reference: http://es5.github.io/#x15.2.3.5
+  Object.create = (function() {
+    // To save on memory, use a shared constructor
+    function Temp() {}
 
-//A rajouter :
-//multii={1:'bouture',2:'division',3:'marcottage',4:'rejet',5:'semis',6:'greffe'};
-//nomlatin: {descr:'nom latin',type:'text',clr:'white',img:'' } ,
-	// moistaille: {descr:'Mois de taille',type:'mois',clr:'#D8D5D5',img:''} ,
-	//moisflo: {descr:'Mois de floraison',type:'mois',clr:'#D8D5D5',img:''} ,
+    // make a safe reference to Object.prototype.hasOwnProperty
+    var hasOwn = Object.prototype.hasOwnProperty;
+
+    return function (O) {
+      // 1. If Type(O) is not Object or Null throw a TypeError exception.
+      if (typeof O != 'object') {
+        throw TypeError('Object prototype may only be an Object or null');
+      }
+
+      // 2. Let obj be the result of creating a new object as if by the
+      //    expression new Object() where Object is the standard built-in
+      //    constructor with that name
+      // 3. Set the [[Prototype]] internal property of obj to O.
+      Temp.prototype = O;
+      var obj = new Temp();
+      Temp.prototype = null; // Let's not keep a stray reference to O...
+
+      // 4. If the argument Properties is present and not undefined, add
+      //    own properties to obj as if by calling the standard built-in
+      //    function Object.defineProperties with arguments obj and
+      //    Properties.
+      if (arguments.length > 1) {
+        // Object.defineProperties does ToObject on its first argument.
+        var Properties = Object(arguments[1]);
+        for (var prop in Properties) {
+          if (hasOwn.call(Properties, prop)) {
+            obj[prop] = Properties[prop];
+          }
+        }
+      }
+
+      // 5. Return obj
+      return obj;
+    };
+  })();
+}
+lgMois=40;decalage=12;keysSorted='';//largeur d'un mois
+tab={'table':latable};plante={};plantetemp={};
+
 champsdb={
-		'nom':{
-			type:'text',avt:'',aps:'',expl:'nom usuel '},
-		
-		
 		'associe':{
-			type:'plant',expl:'Bons compagnons: ',img:'&#xf164;',width:500},
+			type:'plant',expl:'Bons compagnons: ',img:'&#xf164;',width:500,gpe:'compagnons',vizib:'oui'},
 		'antiassocie':{
-			type:'plant',expl:'Associations néfastes: ',img:'&#xf165;',width:500},
-		'commentaire':{expl:'Commentaire',type:'text',img:'',avt:'',aps:'' } ,
-			conservalimt: {expl:"Conservation de l'aliment(en jours)",type:'nbr',img:'' },	
+			type:'plant',expl:'Associations néfastes: ',img:'&#xf165;',width:500,gpe:'compagnons',vizib:'oui'},
+		'commentaire':{
+			expl:'Commentaire',type:'text',img:'',avt:'',aps:'',gpe:'autres' ,vizib:'oui'} ,
+		'conservalimt': {
+			expl:"Conservation de l'aliment",type:'nbr',img:'' ,gpe:'vies',vizib:'peu',avt:'',aps:'jours'},	
 		'drang':{
-			expl:'espacement dans le rang(cm)',type:'nbr',clr:'#D8D5D5',img:'',avt:'&#xf07e;',aps:'cm'} , 
+			expl:'espacement dans le rang',type:'nbr',clr:'#D8D5D5',img:'',avt:'&#xf07e;',aps:'cm',gpe:'semis',vizib:'peu'} , 
 		'dligne':{
-			expl:'espacement dans la ligne(cm)',type:'nbr',clr:'#D8D5D5',img:'',avt:'&#xf07d;',aps:'cm'} ,
+			expl:'espacement dans la ligne',type:'nbr',clr:'#D8D5D5',img:'',avt:'&#xf07d;',aps:'cm',gpe:'semis',vizib:'oui'} ,
 		
 		'eau':{
-			type:'color',expl:'sol ',clr:["",'#BFBB00','brown','#FF0000'],img:'&#xf1b3;',lgd:["",'sableux/léger','équilibré','argileux/lourd']},
+			type:'color',expl:'sol ',clr:["",'#BFBB00','brown','#FF0000'],img:'&#xf1b3;',lgd:["",'sableux/léger','équilibré','argileux/lourd'],gpe:'sol',vizib:'oui'},
 		'famille':{
-			type:'text',avt:'',aps:'',expl:'famille'},	
+			type:'text',avt:'',aps:'',expl:'famille ',gpe:'noms',vizib:'oui'},	
 		
-		'racine':{
-			type:'picto',expl:'racine ',lgd:["",'pivotante','traçante','fasciculée'],img:["",'Y','W','^']},	
+		
 		'hauteur':{
-			expl:'hauteur ',type:'nbr',avt:'&#xf07d;',aps:'cm'},
+			expl:'hauteur ',type:'nbr',avt:'&#xf07d;',aps:'cm',gpe:'enver',vizib:'oui'},
 		'largeur':{
-			expl:'largeur ',type:'nbr',avt:'&#xf07e;',aps:'cm'},
+			expl:'largeur ',type:'nbr',avt:'&#xf07e;',aps:'cm',gpe:'enver',vizib:'oui'},
+		'moistaille':{type:'mois',img:'T',expl:'Taille ',gpe:'calendar',vizib:'oui'},	
+		'moisflo':{
+			type:'mois',img:'&#xf1e9;',expl:'Floraison ',top:10,gpe:'calendar',vizib:'oui'},
 		'multi':{
-			expl:'multiplication par ',type:'text',avt:'',aps:''},
-			
+			expl:'multiplication par ',type:'multiindex',lgd:['','bouture','division','greffe','marcottage','rejet','semis','semis spontané'],gpe:'vies',vizib:'oui'},
+		'nom':{
+			type:'text',avt:'',aps:'',expl:'nom usuel ',gpe:'noms',vizib:'oui'},
+		'nomlatin':{
+			type:'text',avt:'',aps:'',expl:'nom scientifique ',gpe:'noms',vizib:'peu'},			
 		'pH':{
-			type:'color',clr:['','red','green','blue'],img:'pH',expl:'pH',lgd:["",'acide','neutre','basique']},
+			type:'color',clr:['','red','green','blue'],img:'pH',expl:'pH ',lgd:["",'acide','neutre','basique'],gpe:'sol',vizib:'oui'},
 		
 		
 		'prof':{
-			expl:'profondeur ',type:'nbr',avt:'&#xf175;',aps:'cm'},
-		
+			expl:'profondeur ',type:'nbr',avt:'&#xf175;',aps:'cm',gpe:'semis',vizib:'oui'},
+		'racine':{
+			type:'picto',expl:'racine ',lgd:["",'pivotante','traçante','fasciculée'],img:["",'Y','W','^'],gpe:'solriche',vizib:'oui'},	
 		'recolteDeb':{
-			type:'mois',img:'Rd',expl:'Début de la récolte '},
+			type:'mois',img:'Rd',expl:'Début de la récolte ',gpe:'calendar',vizib:'non'},
 		'recolteFin':{
-			type:'mois',img:'Rf',expl:'Fin de la récolte '},
+			type:'mois',img:'Rf',expl:'Fin de la récolte ',gpe:'calendar',vizib:'non'},
 		'RECOLTE':{
-			type:'periode',debPer:'recolteDeb',finPer:'recolteFin',img:'<img src="img/rcolte.png" alt="récolte">',expl:'Récolte: ',top:30},
+			type:'periode',debPer:'recolteDeb',finPer:'recolteFin',img:'<img src="img/rcolte.png" alt="récolte">',expl:'Récolte: ',top:22,gpe:'calendar',vizib:'oui'},
 		'rendement':{
-			expl:'Nombre de kg/m² lors de la récolte',type:'nbr',img:'' ,avt:'',aps:'kg/m²'},
+			expl:'Nombre de kg/m² lors de la récolte',type:'nbr',img:'' ,avt:'',aps:'kg/m²',gpe:'sol',vizib:'oui'},
 		'repiq':{
-			type:'mois',img:'Rq',expl:'Repiquage ',top:29},
+			type:'mois',img:'Rq',expl:'Repiquage ',top:11,gpe:'calendar',vizib:'oui'},
 		'semisext':{
-			type:'mois',img:'<img src="img/semiext.png" alt="semis exterieur"/>',expl:'Semis extérieur ',top:10},
+			type:'mois',img:'<img src="img/semiext.png" alt="semis exterieur"/>',expl:'Semis extérieur ',top:0,gpe:'calendar',vizib:'oui'},
 		'semisabri':{
-			type:'mois',img:'Sa',expl:'Semis abri ',top:5},
+			type:'mois',img:'Sa',expl:'Semis abri ',top:0,gpe:'calendar',vizib:'oui'},
 		'semisint':{
-			type:'mois',img:'Si',expl:'Semis intérieur ',top:0},
+			type:'mois',img:'Si',expl:'Semis intérieur ',top:0,gpe:'calendar',vizib:'oui'},
 		'solriche':{
-			type:'picto',expl:'besoins en nutriments ',lgd:['','faibles','moyens','élevés'],img:['','&#xf006;','&#xf123;','&#xf005;']},
+			type:'picto',expl:'besoins en nutriments ',lgd:['','faibles','moyens','élevés'],img:['','&#xf006;','&#xf123;','&#xf005;'],gpe:'solriche',vizib:'oui'},
 		
 		'soleil':{
-			type:'picto',expl:'ensoleillement ',lgd:['','important','moyen','faible'],img:["",'&#xf185;','&#xf185;/&#xf0c2;','&#xf0c2;']},
+			type:'picto',expl:'ensoleillement ',lgd:['','important','moyen','faible'],img:["",'&#xf185;','&#xf185;/&#xf0c2;','&#xf0c2;'],gpe:'soleil',vizib:'oui'},
 		
 	
 		'Tmin':{
-			expl:'Température mini=',type:'nbrnul',avt:'&#xf069;',aps:'°C'},
+			expl:'Température mini=',type:'nbrnul',avt:'&#xf069;',aps:'°C',gpe:'soleil',vizib:'oui'},
 		
 		'tpslevee':{
-			expl:'Nombre de jours de la graine à la plantule',type:'nbr',img:'' } ,
-	
-			
+			expl:'Nombre de jours de la graine à la plantule',type:'nbr',img:'',avt:'lève en ',aps:'jours',gpe:'semis',vizib:'peu' } ,
 		 'tpsconserv':{
-			expl:'Durée de conservation des semences (années)',type:'nbr',img:'' } ,
-			
+			expl:'Durée de conservation des semences',type:'nbr',img:'',gpe:'autres',vizib:'oui',avt:'',aps:'ans' } ,
+		'usagemedic':{
+			expl:'usage médical: ',type:'multiindex',lgd:['','Anesthésiant','Antiseptique','Anti-inflammatoire','Astringent','Cœur et vaisseaux','Coupe-faim','Dermatologie','Digestion','Diététique','Diurétique','Insomnie','Lactation','Œdème','Parasites','Poumons','Sédatif','Sclérose','Vulnéraire'],gpe:'util',vizib:'oui'},	
 		'utilisation':{
-			expl:'Autres utilisation de la plante',		type:'text',img:'' } ,
-	// medecine: {descr:'Utilisation médecinale de la plante',type:'medecin',clr:'#54CEAD',img:'' } ,
-		
-			
+			expl:'Utilisations de la plante',type:'multiindex',lgd:['','bois de chauffe','bois d\'oeuvre','construction','couverture hiver','engrais','fertilisateur de sol','insecticide','paillage','refuge d\'insecte','vannerie/textile','parfum','brise-vent'],gpe:'util',vizib:'oui'} ,
 		'type':{
-			expl:'partie comestible de la plante',type:'pictomulti',lgd:['','feuille','fruit','fleur','racine','tige'],img:['','&#xf06c;','&#xf094;','&#xf1e9;','&radic;','I']},
-		
+			expl:'comestible: ',type:'pictomulti',lgd:['','feuille','fruit','fleur','racine','tige/sève'],img:['','&#xf06c;','&#xf094;','&#xf1e9;','&radic;','I'],gpe:'vies',vizib:'oui'},
 		'vie':{
-			type:'nbr',avt:'Vit ',aps:' an(s)',expl:'espérance de vie (ans) '},
-			
+			type:'nbr',avt:'',aps:'an(s)',expl:'espérance de vie ',gpe:'vies',vizib:'oui'},
 		'vivace':{
-			type:'bool',1:'vivace&#xf0e2;',0:"annuelle",expl:'Vivace '
-			}
+			type:'bool',1:'vivace&#xf0e2;',0:"annuelle",expl:'Caractère vivace ',gpe:'vies',vizib:'oui'}
 };
+
+titreschoisis=['noms','vies','calendar','solriche','enver','soleil','compagnons','sol','util','semis','autres'];
+titres={//quelles sont les différentes cases et leurs contenus
+	noms:{
+		desc:'Nom',colorbox:'#EFEFEF'},
+	vies:{
+		desc:'Durée de vie',colorbox:'#54CEAD'},
+	calendar:{
+		desc:'Calendrier',colorbox:'#3EF53E'},
+	solriche:{
+		desc:'Occupation du sol ',colorbox:'#FFA500'},
+	enver:{
+		desc:'Envergure',colorbox:'#F53E82'},
+	soleil:{
+		desc:'Ensoleillement ',colorbox:'#54CEAD'},
+	compagnons:{
+		desc:'compagnonage',colorbox:'#71EAAE'},
+	sol:{
+		desc:'Sol',colorbox:'#E8ADAD'},
+	util:{
+		desc:'Utilité',colorbox:'#E0ADAD'},
+	semis:{
+		desc:'Semis',colorbox:'#D8D5D5'},
+	autres:{
+		desc:'Autres',colorbox:'#E0ADAD'}
+		
+};
+blocss={};
+for(var champ in champsdb){
+				var nombloc=champsdb[champ]['gpe'];
+				if(blocss[nombloc]){}
+				else blocss[nombloc]=[];
+				blocss[nombloc].push(champ);
+}
+
+
+
+
 criteretri={
 	'famille':'text',	
 	'hauteur':'nbr',
@@ -130,26 +184,34 @@ criteretri={
 
 
 criterefiltre={
+	'semisext':{expl:"Semis exterieur",
+		typ:'val',values:{
+			1:"Janvier",2:"Février", 3:"Mars",4:"Avril",5:"Mai",6:"Juin",7:"Juillet",8:"Août",9:"Septembre",10:"Octobre",11:"Novembre",12:"Décembre"}},	
+	'recolte':{expl:"Récolte",
+		typ:'period',deb:'recolteDeb',fin:'recolteFin',values:{1:"Janvier",2:"Février", 3:"Mars",4:"Avril",5:"Mai",6:"Juin",7:"Juillet",8:"Août",9:"Septembre",10:"Octobre",11:"Novembre",12:"Décembre"}},
 	'vivace':{expl:'Vivace?',
 		typ:'val',values:{0:'annuelle',1:'vivace'}},
 	'hauteur':{expl:'Hauteur',
 		typ:'categorie',values:{'0_15':'couvre-sol',
 					'15_100':'petite plante',
 					'100_200':'arbuste',
-					'200_10000':'arbre'}},	
+					'200_10000':'arbre'}},
+	'utilisation':{expl:"Besoin en nutriments",
+				typ:'val',values:{
+					1:'faible',2:'moyen',3:'élevé'}},	
+	'usagemedic':{
+			expl:'Medicinal ',typ:'val',values:{0:'',1:'Anesthésiant',2:'Antiseptique',3:'Anti-inflammatoire',4:'Astringent',5:'Cœur et vaisseaux',6:'Coupe-faim',7:'Dermatologie',8:'Digestion',9:'Diététique',10:'Diurétique',11:'Insomnie',12:'Lactation',13:'Œdème',14:'Parasites',15:'Poumons',16:'Sédatif',17:'Sclérose',18:'Vulnéraire'}},	
+	'utilisation':{
+			expl:'Utilisation ',typ:'multiindex',values:{0:'',1:'bois de chauffe',2:'bois d\'oeuvre',3:'construction',4:'couverture hiver',5:'engrais',6:'fertilisateur de sol',7:'insecticide',8:'paillage',9:'refuge d\'insecte',10:'vannerie/textile'}} ,	
 	'solriche':{expl:"Besoin en nutriments",
 				typ:'val',values:{
 					1:'faible',2:'moyen',3:'élevé'}},
-	'semisext':{expl:"Semis exterieur",
-		typ:'val',values:{
-			1:"Janvier",2:"Février", 3:"Mars",4:"Avril",5:"Mai",6:"Juin",7:"Juillet",8:"Août",9:"Septembre",10:"Octobre",11:"Novembre",12:"Décembre"}},	
-	'recolte':{expl:"Récolte",
-		typ:'period',deb:'recolteDeb',fin:'recolteFin',values:{1:"Janvier",2:"Février", 3:"Mars",4:"Avril",5:"Mai",6:"Juin",7:"Juillet",8:"Août",9:"Septembre",10:"Octobre",11:"Novembre",12:"Décembre"}}
+	
 };
 
 
 
-nvlentree='<h6><span>Nouvelle plante </span><span class="famille">Inconnue</span></h6><div><span class="awsm edit" id="NULL" style="margin-left: 200px;">&#xf044;</span></div>';
+nvlentree='<tr><h6><span>Nouvelle plante </span><span class="famille">Inconnue</span></h6><div><span class="awsm edit" id="NULL" style="margin-left: 200px;">&#xf044;</span></tr>';
 
 mois={0:'',1:"Janvier",2:"Février", 3:"Mars",4:"Avril",5:"Mai",6:"Juin",7:"Juillet",8:"Août",9:"Septembre",10:"Octobre",11:"Novembre",12:"Décembre"};
 
@@ -158,11 +220,12 @@ function generer_chps_acqu(p,pt){
 	comp=(pt!=0)?1:0;
 	//html="";
 	//console.log(p);
-for(var d in titres){
-		clrBx=titres[d]['colorbox'];
-	for(var c in titres[d]['chp']){
+	for(var b in titres){
+	for(var i in blocss[b]){	
+		Chp=blocss[b][i];
+		clrBx=titres[champsdb[Chp]['gpe']]['colorbox'];
 		diff="differe";
-		Chp=titres[d]['chp'][c];
+		
 		propChamp=champsdb[Chp];//console.log(propChamp);
 		if(propChamp['type']!='periode'){
 			w=(propChamp['width'])?'width:'+propChamp['width']+'px;':'';
@@ -205,10 +268,18 @@ for(var d in titres){
 					for(var i in propChamp['lgd']){
 						if(i!=0){html+='<label><input class="chk" type="checkbox" name="'+Chp+'" value="'+propChamp['lgd'][i]+'"';
 						if($.inArray(propChamp['lgd'][i],splited)!=-1)html+=' checked';	
-						html+='>'+propChamp['lgd'][i]+propChamp['img'][i]+'</option>';}
+						html+='>'+propChamp['lgd'][i]+propChamp['img'][i]+'</label>';}
 					} 
 				break;
-				
+				case 'multiindex':
+					splited=p[Chp].split('_');
+					for(var i in propChamp['lgd']){
+						if(i!=0){html+='<label><input class="chk" type="checkbox" name="'+Chp+'" value="'+i+'"';
+						if($.inArray(i,splited)!=-1)html+=' checked';	
+						html+='>'+propChamp['lgd'][i]+'</label>';}
+					} 
+				break;
+
 				case 'bool':
 					html+=(propChamp['img'])?propChamp['img']:'';
 					html+='<select name="'+Chp+'">';
@@ -230,19 +301,11 @@ for(var d in titres){
 					}
 				break;
 				
-				case 'multii':
-				typesss=p[Chp].split('_');
-				for(var i in multii){
-						html+='<label><input class="chk" type="checkbox" name="'+Chp+'" value="'+i+'"';
-						if($.inArray(typlant[i],typesss)!=-1)html+=' checked';	
-						html+='>'+multii[i]+'</label>';
-					}
-				break;
 				
 				case 'plant':
 				autresplantes=p[Chp].split('_');
 				famille=plante[keysSorted[0]]['famille'].toLowerCase();num_famille=0;html2="";html3="";
-				for(k in keysSorted){
+				for(k in keysSortedFamille){
 					if(plante[keysSorted[k]]['famille'].toLowerCase()!=famille){
 						famille=plante[keysSorted[k]]['famille'].toLowerCase();num_famille++;}
 						html2+='<label class="f'+num_famille+'"><input class="chk" type="checkbox" name="'+Chp+'" value="'+plante[keysSorted[k]].ID+'"';
@@ -255,16 +318,15 @@ for(var d in titres){
 				break;
 				
 				default:
+				html+='('+propChamp['aps']+')';
 				html+=(propChamp['img'])?propChamp['img']:'';
-				html+='<input type="text" name="'+Chp+'" placeholder="'+propChamp['expl']+'" value="'+p[Chp]+'">';
+				html+='<input type="text" name="'+Chp+'" placeholder="'+propChamp['expl']+'('+propChamp['aps']+')" value="'+p[Chp]+'">';
 				break;
 				
-		}
+			}
 		html+='</div>';
-	}
-	}
-	
-		
+		}
+	}	
 	}
 }	
 
@@ -278,6 +340,7 @@ function recup_donnees($obj,typ){
 				value=$obj.find('select option:selected').attr('value');
 			break;
 			case 'pictomulti':
+			case 'multiindex':
 				$obj.find('input:checked').each(function(index){
 					value+=$(this).attr('value')+'_';
 				});
